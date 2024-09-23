40bfa651f8707e8875b566db7c78207d8a16d75f
Marcos <marcos.wakuluk@wuala.net>
2024-09-23 00:00:38 -0300

    emails and mercadopago

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
 const express = require("express");
 const cors = require("cors");
-const { Resend } = require("resend");
-require('dotenv').config();
+const { MercadoPagoConfig, Preference } = require("mercadopago");
+require("dotenv").config();
 
 const app = express();
 let port = process.env.PORT || 3100;
 
-// Middleware
+const client = new MercadoPagoConfig({ accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN });
+
 app.use(cors());
 app.use(express.json());
 
-// Route to send email
 app.post("/send-email", async (req, res) => {
   try {
     const { email, name, lastName } = req.body;
-
     const resend = new Resend(process.env.RESEND_API_KEY);
 
-    // Send email
     const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: "onboarding@resend.dev",
       to: email,
-      subject: 'Welcome to KM Pro Fitness',
-      html: `<strong>Welcome ${name} ${lastName} to your KM Pro Fitness plan</strong>`
+      subject: "Welcome to KM Pro Fitness",
+      html: `<strong>Welcome ${name} ${lastName} to your KM Pro Fitness plan</strong>`,
     });
 
     res.status(200).json({
       status: "success",
-      data: { data }
+      data: { data },
     });
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      status: 'error',
-      message: 'An error occurred while sending the email'
+      status: "error",
+      message: "An error occurred while sending the email",
+    });
+  }
+});
+
+app.post("/create-preference", async (req, res) => {
+  const { items } = req.body;
+
+  try {
+    const preference = new Preference(client);
+    const preferenceData = await preference.create({
+      body: {
+        items: items,
+      },
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: preferenceData,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "An error occurred while creating the payment preference",
     });
   }
 });
